99f2f30501ef9443573a76b9b1938a9faffe2dba
Tamás B. Nemes <tb.nemes@gmail.com>
2023-07-24 18:30:28 +0200

    More setup

--- smort/src/app/app.module.ts
+++ smort/src/app/app.module.ts
@@ -1,15 +1,25 @@
+//#region Basics
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+//#endregion
+
+//#region Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { UploadComponent } from './upload/upload.component';
 import { SettingsComponent } from './settings/settings.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ProfileComponent } from './profile/profile.component';
+//#endregion
+
+//#region Video Streaming
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
+//#endregion
 
 @NgModule({
   declarations: [
@@ -19,14 +29,14 @@ import { ProfileComponent } from './profile/profile.component';
     HomeComponent,
     UploadComponent,
     SettingsComponent,
-    UserProfileComponent,
-    ProfileComponent
+    ProfileComponent,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
