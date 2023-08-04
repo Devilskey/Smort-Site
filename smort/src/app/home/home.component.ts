ba0dd74f703f831ab4034767d83d8ac1bfad3c2e
Tamás B. Nemes <tb.nemes@gmail.com>
2023-08-04 17:21:48 +0200

    Trying to get video player to work properly

--- smort/src/app/home/home.component.ts
+++ smort/src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-
-//#region Video Streaming
-import { VgApiService } from '@videogular/ngx-videogular/core';
-//#endregion
+import { Video, Videos } from '../../assets/videos';
 
 @Component({
   selector: 'app-home',
@@ -10,17 +7,59 @@ import { VgApiService } from '@videogular/ngx-videogular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  preload: string = 'auto';
-  api: VgApiService = new VgApiService();
+  videos = Videos;
+  videoIndex = 0;
+  currentVideo = this.videos[this.videoIndex];
+  data: any;
 
-  OnPlayerReady(source: VgApiService) {
-    this.api = source;
-    this.api
+  OnPlayerReady(data: any) {
+    this.data = data;
+    this.data
       .getDefaultMedia()
       .subscriptions.loadedMetadata.subscribe(this.AutoPlay.bind(this));
+    this.data
+      .getDefaultMedia()
+      .subscriptions.ended.subscribe(this.NextVideo.bind(this));
+    //   this.data
+    //     .getDefaultMedia()
+    //     .subscriptions.ended.subscribe(this.PreviousVideo.bind(this));
   }
 
   AutoPlay() {
-    this.api.play();
+    this.data.play();
   }
+
+  // PreviousVideo() {
+  //   this.videoIndex--;
+  //   if (this.videoIndex <= 0) {
+  //     this.videoIndex = 0;
+  //   }
+  //   console.log('previous');
+  // console.log(this.videoIndex);
+  // console.log(this.videos[this.videoIndex].url);
+  // console.log(this.currentVideo);
+  // console.log(this.currentVideo.url);
+  // }
+
+  NextVideo() {
+    this.videos[this.videoIndex] = this.currentVideo;
+    this.videoIndex++;
+    if (this.videoIndex === this.videos.length) {
+      this.videoIndex = 0;
+    }
+    this.currentVideo = this.videos[this.videoIndex];
+    console.log('next');
+    console.log(this.videoIndex);
+    console.log(this.currentVideo);
+    console.log(this.currentVideo.url);
+  }
+
+  // get some available videos from people you follow
+
+  // Randomise them
+
+  // Save next video
+
+  // Save previous
+  // If there is no previous, don't allow switching to it
 }
